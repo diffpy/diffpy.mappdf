e5262274cc98a3d23db0fabb40ecd46c6938816f
christopher <cjwright4242@gmail.com>
2017-12-21 15:08:49 -0500

    STY: pep8

--- demo/utils.py
+++ demo/utils.py
@@ -9,18 +9,19 @@ import seaborn.apionly as sns
 from diffpy.pdfgetx.pdfgetter import PDFGetter
 from diffpy.pdfgetx.pdfconfig import PDFConfig
 
+
 def load_chi(lib_dir):
     """method to load chi files from lib_dir"""
-    chi_fn_list = sorted([f for f in os.listdir(lib_dir) if\
+    chi_fn_list = sorted([f for f in os.listdir(lib_dir) if \
                           f.endswith('.chi')])
     Iq_list = []
     Q_list = []
     for chi in chi_fn_list:
         fn = os.path.join(lib_dir, chi)
         try:
-            array = np.loadtxt(fn).T # pyFAI
+            array = np.loadtxt(fn).T  # pyFAI
         except:
-            array = np.loadtxt(fn, skiprows=4).T # fit2D
+            array = np.loadtxt(fn, skiprows=4).T  # fit2D
         Q, Iq = array
         Iq_list.append(Iq)
         Q_list.append(Q)
@@ -28,7 +29,7 @@ def load_chi(lib_dir):
     Q_array = np.asarray(Q_list)
     Iq_array = np.asarray(Iq_list)
     print("INFO: load Q_array.shape = {}, Iq_array.shape = {}"
-          .format(Q_array.shape, Iq_array.shape))
+        .format(Q_array.shape, Iq_array.shape))
 
     return chi_fn_list, Q_array, Iq_array
 
@@ -88,16 +89,17 @@ def Gr_transform(q_grid, Iq_array, composition_info, config_dict):
     # iterate through pairs
     Gr_list = []
     for Iq, compo in zip(_Iq_array, composition_info):
-        config_dict.update({'composition':compo})
+        config_dict.update({'composition': compo})
         pdfconfig.update(**config_dict)
         pdfgetter = PDFGetter(pdfconfig)
         r, Gr = pdfgetter(x=q_grid, y=Iq)
         Gr_list.append(Gr)
     Gr_array = np.asarray(Gr_list)
     print("INFO: finish transform. output Gr shape is {}"
-          .format(Gr_array.shape))
+        .format(Gr_array.shape))
 
     return r, Gr_array
+
 
 def conf_label_size(ax, label_size):
     ax.xaxis.label.set_size(label_size)
@@ -106,35 +108,36 @@ def conf_label_size(ax, label_size):
 
 def conf_tick_size(ax, tick_size):
     for tick in ax.xaxis.get_major_ticks():
-                tick.label.set_fontsize(tick_size)
+        tick.label.set_fontsize(tick_size)
     for tick in ax.yaxis.get_major_ticks():
-                tick.label.set_fontsize(tick_size)
-            
+        tick.label.set_fontsize(tick_size)
 
-def bSeabornStyle(ticks=False, cycle='simon', context='notebook', f_scale=1, a_scale=1,
+
+def bSeabornStyle(ticks=False, cycle='simon', context='notebook', f_scale=1,
+                  a_scale=1,
                   l_width=1, m_size=1, xt_col='black'):
     sns.reset_orig()
-    if ticks==False:
+    if ticks == False:
         sns.set_style("whitegrid")
     else:
         sns.set_style("ticks")
 
     sns.set_style({
-    'grid.linestyle': '--',
-    'grid.color': 'b2b2b2',
-    'axes.linewidth': (a_scale*1.75),
-    'axes.labelcolor':'black',
-    'axes.edgecolor': 'black',
-    'xtick.color': xt_col,
-    'ytick.color': 'black',
-    'xtick.direction': 'in',
-    'ytick.direction': 'in',
+        'grid.linestyle': '--',
+        'grid.color': 'b2b2b2',
+        'axes.linewidth': (a_scale * 1.75),
+        'axes.labelcolor': 'black',
+        'axes.edgecolor': 'black',
+        'xtick.color': xt_col,
+        'ytick.color': 'black',
+        'xtick.direction': 'in',
+        'ytick.direction': 'in',
     })
 
-    sns.set_context(context, font_scale=(f_scale*1.5),
-                    rc={"lines.linewidth": (l_width*2),
-                        "lines.markersize":(m_size*5),})
-    mpl.rcParams['font.family']='Arial'
+    sns.set_context(context, font_scale=(f_scale * 1.5),
+                    rc={"lines.linewidth": (l_width * 2),
+                        "lines.markersize": (m_size * 5), })
+    mpl.rcParams['font.family'] = 'Arial'
     mpl.rcParams['mathtext.fontset'] = 'stixsans'
     mpl.rcParams['font.size'] = 18
     mpl.rcParams['figure.dpi'] = 150
@@ -142,14 +145,16 @@ def bSeabornStyle(ticks=False, cycle='simon', context='notebook', f_scale=1, a_s
     mpl.rcParams['grid.linewidth'] = 0.5
     mpl.rcParams['savefig.bbox'] = 'tight'
 
-
-    if cycle=='simon':
-        simonCycle = ["#0B3C5D", "#062F4F", "#328CC1", "#D9B310", "#984B43", "#B82601",
-                 "#57652A", "#76323F", "#626E60", "#AB987A", "#C09F80", "#b0b0b0ff"]
+    if cycle == 'simon':
+        simonCycle = ["#0B3C5D", "#062F4F", "#328CC1", "#D9B310", "#984B43",
+                      "#B82601",
+                      "#57652A", "#76323F", "#626E60", "#AB987A", "#C09F80",
+                      "#b0b0b0ff"]
         sns.set_palette(simonCycle)
-    elif cycle=='simon2':
-        simonCycle2 = ["#0B3C5D", "#B82601",  "#1c6b0a","#328CC1", "#062F4F", "#D9B310", "#984B43",
-                 "#76323F", "#626E60", "#AB987A", "#C09F80", "#b0b0b0ff"]
+    elif cycle == 'simon2':
+        simonCycle2 = ["#0B3C5D", "#B82601", "#1c6b0a", "#328CC1", "#062F4F",
+                       "#D9B310", "#984B43",
+                       "#76323F", "#626E60", "#AB987A", "#C09F80", "#b0b0b0ff"]
         sns.set_palette(simonCycle2)
     else:
         # Use another SNS preset: deep, muted, bright, pastel, dark, colorblind
